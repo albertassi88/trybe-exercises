1f4ceae73dc5be7249ecc5d28b2855449991abae
Ruben Albertassi <albertassi88@gmail.com>
2020-10-27 18:31:46 -0300

    Exercicios Bonus

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/bonus2.js
@@ -0,0 +1,16 @@
+/*Ordene o array numbers em ordem decrescente e imprima seus valores;
+*/
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (x=0; x<numbers.length; x++){
+    for (j=0; j<numbers.length; j++){
+        if (numbers[x] > numbers[j]){
+            let posicao = numbers[x];
+            numbers[x] = numbers[j];
+            numbers[j] = posicao;
+        }
+    }
+}
+
+console.log(numbers);
