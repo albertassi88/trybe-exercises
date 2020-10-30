0da2b28502a30e10c9bb0481369ef3a35ac182e7
Ruben Albertassi <albertassi88@gmail.com>
2020-10-30 19:20:06 -0300

    Conclusao do exercicio

--- exercises/javascript-dom-eventos-e-web-storage_1/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_1/script.js
@@ -1,11 +1,38 @@
-  /* Aqui você vai modificar os elementos já existentes utilizando apenas as funções:
-        - document.getElementById()
-        - document.getElementsByClassName()
-        - document.getElementsByTagName()
- Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
- Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
- Crie uma função que mude a cor do quadrado vermelho para branco.
- Crie uma função que corrija o texto da tag <h1>.
- Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
- Crie uma função que exiba o conteúdo de todas as tags <p> no console.
-*/
+function mudarTexto(texto){
+    let p = document.querySelector(".center-content p").innerText = texto;
+}
+mudarTexto("Se Deus quiser eu me vejo trabalhando com desenvolvimento de software em uma grande empresa.");
+
+
+function mudarCor1(cor){
+    let c = document.querySelector(".main-content").style.backgroundColor = cor; 
+}
+mudarCor1("rgb(76,164,109)");
+
+
+function mudarCor2(cor){
+    let c = document.querySelector(".main-content .center-content").style.backgroundColor = cor; 
+}
+mudarCor2("white");
+
+
+function texto(textoCorreto){
+    let h1 = document.querySelector(".title").innerHTML = textoCorreto;
+}
+texto("Exercício 5.1 - JavaScript");
+
+
+function textoMaiusculo(texto){
+    let p = document.querySelectorAll(".center-content p");
+    for (let x=0; x<p.length; x++){
+        p[x].style.textTransform = texto;
+    }    
+}
+textoMaiusculo("uppercase");
+
+
+function consoleP(){
+    let p = document.querySelectorAll("p")
+    return p;
+}
+console.log(consoleP());
