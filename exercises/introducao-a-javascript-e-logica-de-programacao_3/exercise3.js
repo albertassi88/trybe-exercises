c2ea8d6c0ee3d173c1301f6c28b64d0417214214
Ruben Albertassi <albertassi88@gmail.com>
2020-11-02 15:18:09 -0300

    Alteracao no exercicio 3

--- exercises/introducao-a-javascript-e-logica-de-programacao_3/exercise3.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercise3.js
@@ -3,17 +3,16 @@
 
 let n = 5;
 
-
-for (let linha=0; linha<=n; linha++){
-    let triangulo = "";
-    for (let coluna=0; coluna<n; coluna++){
-        if (coluna < n - linha){
-            triangulo = triangulo + " ";    
-        }else{
-            triangulo = triangulo + "*";
-        }
-    }        
-    console.log(triangulo);
+for (let linha=0; linha<n; linha++){
+   let triangulo = "";
+   for (let coluna=0; coluna<=n; coluna++){
+       if (coluna < (n - linha)){
+        triangulo += " "; 
+       }else {
+        triangulo += "*";
+       }    
+ }
+ console.log(triangulo);
 }
 
 
