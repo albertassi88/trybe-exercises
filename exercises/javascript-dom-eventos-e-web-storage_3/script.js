3218c412853bf1d535ec93a2bb47e350d12d1817
Ruben Albertassi <albertassi88@gmail.com>
2020-11-07 10:37:34 -0300

    Inclusao do exercicio 4

--- exercises/javascript-dom-eventos-e-web-storage_3/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_3/script.js
@@ -88,5 +88,17 @@ dayCor.addEventListener("click", function(){
 });
 
 
+/*4-Implemente uma função que receba como parâmetro a string "Sexta-feira" e crie dinamicamente um 
+botão com o nome "Sexta-feira".
+Adicione a este botão o ID "btn-friday".
+Adicione este botão como filho/filha da tag <div> com classe "buttons-container".*/
 
+let divBtContainer = document.querySelector(".buttons-container");
 
+function friday(){
+    let btnFriday = document.createElement("button");
+    btnFriday.innerHTML = "Sexta-feira";
+    btnFriday.id = "btn-friday";
+    divBtContainer.appendChild(btnFriday);
+}
+friday();
