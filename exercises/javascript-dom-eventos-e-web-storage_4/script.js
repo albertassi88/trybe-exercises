0ba02ee64e14ea6456c43e35b9f42f3d7783d9f1
Ruben Albertassi <albertassi88@gmail.com>
2020-11-08 16:57:35 -0300

    Inclusao das funcoes btcolor, bttexto, bttamanho, btespacamento, btfonte

--- exercises/javascript-dom-eventos-e-web-storage_4/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_4/script.js
@@ -32,24 +32,103 @@ function placeHolder(){
 placeHolder();
 
 
-let buttonId = document.querySelector("#botao0");
-let inputText = document.querySelector("#texto0");
+let buttonId1 = document.querySelector("#botao0");
+let inputText1 = document.querySelector("#texto0");
+let buttonId2 = document.querySelector("#botao1");
+let inputText2 = document.querySelector("#texto1");
+let buttonId3 = document.querySelector("#botao2");
+let inputText3 = document.querySelector("#texto2");
+let buttonId4 = document.querySelector("#botao3");
+let inputText4 = document.querySelector("#texto3");
+let buttonId5 = document.querySelector("#botao4");
+let inputText5 = document.querySelector("#texto4");
+let buttonId6 = document.querySelector("#botao5");
+let inputText6 = document.querySelector("#texto5");
 
 
 function btName(){
-    buttonId.addEventListener("click", function(){      
-        let result = "";
-        
-        if (inputText.value != ""){
-            result = inputText.value;              
+    buttonId1.addEventListener("click", function(){      
+        let result = "";        
+        if (inputText1.value != ""){
+            result = inputText1.value;              
         }else{
             alert("Erro! Favor digitar um nome.");
         }        
-        inputText.value = "";    
+        inputText1.value = "";    
         localStorage.setItem("nome", result);         
     });    
 }
 btName();
+
+function btColor(){
+    buttonId2.addEventListener("click", function(){      
+        let result = "";        
+        if (inputText2.value != ""){
+            result = inputText2.value;              
+        }else{
+            alert("Erro! Favor digitar uma cor.");
+        }        
+        inputText2.value = "";    
+        localStorage.setItem("cor", result);         
+    });    
+}
+btColor();
+
+function btTexto(){
+    buttonId3.addEventListener("click", function(){      
+        let result = "";        
+        if (inputText3.value != ""){
+            result = inputText3.value;              
+        }else{
+            alert("Erro! Favor digitar uma cor para o texto.");
+        }        
+        inputText3.value = "";    
+        localStorage.setItem("texto", result);         
+    });    
+}
+btTexto();
+
+function btTamanho(){
+    buttonId4.addEventListener("click", function(){      
+        let result = "";        
+        if (inputText4.value != ""){
+            result = inputText4.value;              
+        }else{
+            alert("Erro! Favor digitar um tamanho para a fonte.");
+        }        
+        inputText4.value = "";    
+        localStorage.setItem("tamanho", result);         
+    });    
+}
+btTamanho();
+
+function btEspacamento(){
+    buttonId5.addEventListener("click", function(){      
+        let result = "";        
+        if (inputText5.value != ""){
+            result = inputText5.value;              
+        }else{
+            alert("Erro! Favor digitar um tamanho para o espaçamento.");
+        }        
+        inputText5.value = "";    
+        localStorage.setItem("espaco", result);         
+    });    
+}
+btEspacamento();
+
+function btFonte(){
+    buttonId6.addEventListener("click", function(){      
+        let result = "";        
+        if (inputText6.value != ""){
+            result = inputText6.value;              
+        }else{
+            alert("Erro! Favor digitar um tipo de fonte.");
+        }        
+        inputText6.value = "";    
+        localStorage.setItem("fonte", result);         
+    });    
+}
+btFonte();
 
 let name = localStorage.getItem("nome"); 
 let h2 = document.createElement("h2");
