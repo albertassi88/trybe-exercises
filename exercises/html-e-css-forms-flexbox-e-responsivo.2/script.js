c07fa3e7ef384184507170d576c892eac75f1db9
Ruben Albertassi <albertassi88@gmail.com>
2020-11-14 21:35:51 -0300

    Alteracao no enunciado do exercicio

--- exercises/html-e-css-forms-flexbox-e-responsivo.2/script.js
+++ exercises/html-e-css-forms-flexbox-e-responsivo.2/script.js
@@ -1,11 +1,16 @@
-/*1-Crie um <fieldset> para os seguintes dados pessoais:
-Nome - Texto, Limite de 40 caracteres, Campo obrigatório, Email - Texto, Limite de 50 caracteres,
-Campo obrigatório, CPF - Texto, Limite de 11 caracteres, Campo obrigatório, Endereço - Texto,
-Limite de 200 caracteres, Campo obrigatório, Cidade - Texto, Limite de 28 caracteres,
-Campo obrigatório, 
-Estado ComboBox, Todos os estados do Brasil, Utilize estruturas de repetição via JavaScript 
-para gerar os <option>, Campo obrigatório
-Tipo - Radio Button, Casa, Apartamento, Campo obrigatório*/
+/*Adicione um framework CSS de sua escolha ao formulário que você construiu na última aula e 
+o utilize para estilizar o formulário.
+Sugestões: Bulma, Bootstrap, Semantic UI e Materialize.
+
+Adicione uma biblioteca JavaScript de date picker ao formulário que você construiu na última 
+aula. Utilize essa biblioteca no campo "Data de início" do formulário. Você pode remover as 
+validações de data que adicionou, uma vez que a biblioteca se encarregará de permitir somente 
+datas válidas.
+Sugestões: DatePickerX e Pickaday.
+
+Adicione uma biblioteca JavaScript de validações ao formulário que você construiu na última 
+aula. Utilize essa biblioteca para substituir as validações que você fez manualmente.
+Sugestões: Just-validate e popup-validation*/
 
 let body = document.querySelector("body");
 let form = document.querySelector("form");
@@ -111,21 +116,6 @@ fieldset.appendChild(comboBox);
 fieldset.appendChild(fieldRadio);
 form.appendChild(fieldset);
 divCadastro.appendChild(fieldset);
-
-
-/*2-Crie outro <fieldset> para dados do seu último emprego, Resumo do currículo - TextArea,
-Limite de 1000 caracteres, Campo obrigatório, Cargo - Texto, Limite de 40 caracteres,
-Campo obrigatório, Descrição do cargo - Texto, Limite de 500 caracteres, Campo obrigatório,
-Data de início - Texto, Verificar o formato da data dd/mm/aaaa.
-O dia deve ser > 0 e <= 31, O mês deve ser > 0 e <= 12, O ano não pode ser negativo.
-Caso alguma das condições for inválida no momento do envio do formulário, 
-exibir mensagem de erro contextualizada, Campo obrigatório*/
-
-
-/*3-Logo abaixo do formulário, crie um botão que:
-Chame uma função JavaScript e interrompa o fluxo automático do form utilizando o preventDefault().
-Execute as validações que foram pedidas ao longo da montagem do formulário.
-Monte uma <div> com o consolidado dos dados que foram inseridos no formulário.*/
 
 let botao = document.querySelector("#botao");
 let fieldTextArea = document.querySelector("#fieldTextArea");
