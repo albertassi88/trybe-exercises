81acdd8eaf8417b32e1375294ac65cfaa29c325e
Ruben Albertassi <albertassi88@gmail.com>
2020-11-24 18:54:42 -0300

    Criacao do exercicio parte 2 do 1 ao 5

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_2/script.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_2/script.js
@@ -42,21 +42,77 @@ const order = {
     payment: {
       total: 60,
     },
-  };
+};
   
-  const customerInfo = (order) => {
+const customerInfo = (order) => {
     
-    console.log(`Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R. ${order.address.street}, Nº: ${order.address.number}, AP: ${order.address.apartment} `)
-
-  }  
-  customerInfo(order);
+    console.log(`Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R. ${order.address.street}, Nº: ${order.address.number}, AP: ${order.address.apartment} `);
+}  
+customerInfo(order);
   
-  const orderModifier = (order) => {
+const orderModifier = (order) => {
 
     order.name = "Luiz Alves";
     order.order.drinks.coke.price = 5;    
-    console.log(`Olá ${order.name}, o total do seu pedido de muzzarella, calabresa e Coca-Cola Zero é R$ ${order.order.pizza.margherita.price + order.order.pizza.pepperoni.price + order.order.drinks.coke.price},00`)
+    console.log(`Olá ${order.name}, o total do seu pedido de muzzarella, calabresa e Coca-Cola Zero é R$ ${order.order.pizza.margherita.price + order.order.pizza.pepperoni.price + order.order.drinks.coke.price},00`);  
+}  
+orderModifier(order);  
+
+
+/* Parte 2*/
+
+
+const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+};
   
-  }  
-  orderModifier(order);
+const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+};
   
+const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+};
+
+/*Crie uma função para adicionar o turno da manhã na lesson2. Essa função deve possuir três 
+parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor 
+dela.*/
+
+const turnoLesson2 = (objeto, chave, valor) => (objeto[chave] = valor);
+
+turnoLesson2(lesson2, "turno", "manhã");
+console.log(lesson2);
+
+/*Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como 
+parâmetro.*/
+
+const keysObjeto = objeto => console.log(Object.keys(objeto));
+keysObjeto(lesson3);
+
+/*Crie uma função para mostrar o tamanho de um objeto.*/
+
+const tamanho = objeto => Object.values(objeto).length;
+console.log(tamanho(lesson1));
+
+/*Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como 
+parâmetro.*/
+
+const valuesObject = (objeto) => Object.values(objeto);
+console.log(valuesObject(lesson2));
+
+/*Crie um objeto de nome allLessons, que deve agrupar todas as aulas através do Object.assign.
+Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1, lesson2 e lesson3. 
+Ao executar o comando console.log(allLessons), a saída deverá ser a seguinte:*/
+
+const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
+console.log(allLessons);
+
+
