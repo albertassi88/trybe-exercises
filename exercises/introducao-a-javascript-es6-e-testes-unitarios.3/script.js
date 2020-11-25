9c45370af22475b39a31cca99affda1dd4815cb5
Ruben Albertassi <albertassi88@gmail.com>
2020-11-25 19:33:47 -0300

    Criacao do enunciado para o exercicio 3

--- exercises/introducao-a-javascript-es6-e-testes-unitarios.3/script.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios.3/script.js
@@ -51,3 +51,15 @@ myRemove(array, 4);
 assert.deepStrictEqual(array, [1, 2, 3, 4]);
 
 assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);  //d)
+
+
+/*3 - A função myRemoveWithoutCopy(arr, item) recebe um array arr e retorna o próprio array sem 
+o elemento item caso ele exista no array
+a) Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) retorna o array esperado
+b) Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+c) Faça uma chamada para a função myRemoveWithoutCopy e verifique se o array passado por 
+parâmetro sofreu alterações
+d) Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 5) retorna o array esperado*/
+
+
+
