955f907958f3b13dfbae39a104b1ba9687c43d70
Ruben Albertassi <albertassi88@gmail.com>
2020-11-28 12:03:53 -0300

    Criacao do exercicio 6

--- exercises/introducao-a-javascript-es6-e-testes-unitarios.3/script.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios.3/script.js
@@ -127,3 +127,34 @@ const obj3 = {
 assert.deepStrictEqual(obj1, obj2);
 assert.notDeepStrictEqual(obj1, obj3);
 assert.notDeepStrictEqual(obj2, obj3);
+
+
+/*6 - Praticando TDD
+Nessa parte os exercícios estão divididos em dois grupos: primeiro, você vai escrever código 
+baseado nos testes. Depois você lerá um código e o que ele tem que trazer de resposta. A partir 
+disso, você escreverá testes e os usará de base para alterar o código. Como assim? Bem, vamos 
+passo a passo!
+Escrevendo código para testes
+Dados os casos de testes abaixo, escreva as funções de forma a passar nos testes. É importante 
+nunca alterar os testes ou as variáveis já escritas no código :
+Escreva a função addOne para passar nos testes já implementados.*/
+
+const assert = require('assert');
+// escreva a função addOne aqui
+
+const addOne = (myArray) => {
+  const newArray = [];
+  for (let index=0; index<myArray.length; index++){
+    newArray.push(myArray[index] + 1);
+  }
+  return newArray;
+};
+
+const myArray = [31, 57, 12, 5];
+const unchanged = [31, 57, 12, 5];
+const expected = [32, 58, 13, 6];
+const output = addOne(myArray);
+
+assert.strictEqual(typeof addOne, 'function');
+assert.deepStrictEqual(output, expected);
+assert.deepStrictEqual(myArray, unchanged);
