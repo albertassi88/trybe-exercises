cdd98a5a62249fdfe0c1cbe0d01dcf2b74d9be8c
Ruben Albertassi <albertassi88@gmail.com>
2020-12-01 19:05:15 -0300

    Criacao do exercicio 4

--- exercises/higher-order-functions-do-javascript-es6.2/exercises4.js
+++ exercises/higher-order-functions-do-javascript-es6.2/exercises4.js
@@ -1,5 +1,6 @@
-/*3 - Crie um array com todos os objetos que possuem gênero ficção científica ou fantasia.
-Dica: use as função filter ;*/
+/*4 - Crie um array ordenado pelos livros com mais de 60 anos de publicação e ordene-o pelo 
+livro mais velho.
+Dica: use as funções filter e sort*/
 
 const assert = require('assert');
 
@@ -67,50 +68,31 @@ const books = [
 ];
 
 const expected_result = [
- {
-    id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    genre: 'Fantasia',
-    author: {
-      name: 'George R. R. Martin',
-      birthYear: 1948
-    },
-    releaseYear: 1991,
-  },
   {
-    id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
-    author: {
-      name: 'J. R. R. Tolkien',
-      birthYear: 1892,
-    },
-    releaseYear: 1954,
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928
   },
   {
     id: 3,
     name: 'Fundação',
     genre: 'Ficção Científica',
-    author: {
-      name: 'Isaac Asimov',
-      birthYear: 1920,
-    },
-    releaseYear: 1951,
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951
   },
   {
-    id: 4,
-    name: 'Duna',
-    genre: 'Ficção Científica',
-    author: {
-      name: 'Frank Herbert',
-      birthYear: 1920,
-    },
-    releaseYear: 1965,
-  },
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954
+  }
 ]
 
-const fantasyOrScienceFiction = () => {
-    return books.filter((book) => book.genre === 'Ficção Científica' || book.genre === 'Fantasia');
-}
+const oldBooks = () => {
+    return books.filter((book) => book.releaseYear < 1960).sort((a,b) => a.releaseYear - b.releaseYear);
+};
 
-assert.deepStrictEqual(fantasyOrScienceFiction(), expected_result);
+assert.deepStrictEqual(oldBooks(), expected_result);
