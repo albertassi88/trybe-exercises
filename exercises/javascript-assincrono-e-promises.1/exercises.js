6971f561260c47f79b754f0a2c5bf60e935fe961
Ruben Albertassi <albertassi88@gmail.com>
2020-12-08 19:04:48 -0300

    Criacao da parte 5

--- exercises/javascript-assincrono-e-promises.1/exercises.js
+++ exercises/javascript-assincrono-e-promises.1/exercises.js
@@ -1,4 +1,5 @@
-//1 - Dado o código abaixo, qual a ordem de finalização de execução das linhas comentadas?
+/*Parte 1
+Dado o código abaixo, qual a ordem de finalização de execução das linhas comentadas?*/
 
 
 const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
@@ -33,7 +34,8 @@ console.log(planetDistanceFromSun(venus)); // B
 console.log(planetDistanceFromSun(jupiter)); // C
 
 
-//2 - Agora, dado o código abaixo, qual a ordem de finalização de execução das linhas comentadas?
+/*Parte 2
+Agora, dado o código abaixo, qual a ordem de finalização de execução das linhas comentadas?*/
 
 
 const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
@@ -68,7 +70,9 @@ setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B
 setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C
 
 
-//3 - A função getPlanet abaixo imprime o planeta Marte de forma síncrona. Modifique getPlanet , de forma que Marte seja impresso assincronamente, depois de 4 segundos.
+/*Parte 3
+A função getPlanet abaixo imprime o planeta Marte de forma síncrona. Modifique getPlanet de 
+forma que Marte seja impresso assincronamente, depois de 4 segundos.*/
 
 
 const getPlanet = () => {
@@ -80,12 +84,16 @@ const getPlanet = () => {
       },
     };
     console.log("Returned planet: ", mars);
-  };
+};
   
- setTimeout(getPlanet, 4000); // imprime Marte depois de 4 segundos
+setTimeout(getPlanet, 4000); // imprime Marte depois de 4 segundos
 
 
- //4 - Suponha que você precise simular uma mensagem enviada do robô Curiosity de Marte para a Terra. O Curiosity envia para a Terra a temperatura atual em Marte, gastando um tempo variável de até 5 segundos para que termine o envio. Crie a função sendMarsTemperature , que imprime a temperatura em Marte.
+/*Parte 4
+Suponha que você precise simular uma mensagem enviada do robô Curiosity de Marte para a Terra. 
+O Curiosity envia para a Terra a temperatura atual em Marte, gastando um tempo variável de até 5 
+segundos para que termine o envio. Crie a função sendMarsTemperature , que imprime a temperatura 
+em Marte.*/
 
 
 const messageDelay = () => Math.floor(Math.random() * 5000);
@@ -102,3 +110,31 @@ const sendMarsTemperature = () => {
 };
 
 sendMarsTemperature();// imprime "Mars temperature is: 20 degree Celsius", por exemplo
+
+
+/*Parte 5
+Agora que você fez a função que envia a temperatura de Marte, suponha que você consiga enviar 
+para o robô Curiosity o que você deseja fazer, uma vez obtida com sucesso a temperatura em Marte. 
+Logo, adicione na função sendMarsTemperature um callback que contenha as ações a serem tomadas 
+em cima da temperatura.*/
+
+
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
+const temperatureInFahrenheit = (temperature) => console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+// definição da função sendMarsTemperature...
+const sendMarsTemperature = (onSuccess) => {
+    const currentTemperature = getMarsTemperature();
+    setTimeout(() => onSuccess(currentTemperature), messageDelay());
+};
+
+sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
+sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
