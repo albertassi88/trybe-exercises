c47bde8f9ad40008528617c127dd1149ce587416
albertassi88 <albertassi88@gmail.com>
2021-01-06 19:28:37 -0300

    Criacao do exercicio 3

--- exercises/testes-automatizados-com-jest.3/exercicio1.test.js
+++ exercises/testes-automatizados-com-jest.3/exercicio1.test.js
@@ -28,3 +28,29 @@ test('Teste do exercicio 2.', () => {
   expect(randomNumber).toBeCalledTimes(1);
   expect(randomNumber).toHaveBeenCalledWith(40, 4);  
 });
+
+
+/*3 - Ainda com a mesma função do primeiro exercício, utilizando o mock, crie uma nova implementação que receba três parâmetros e retorne sua multiplicação. 
+Após fazer os devidos testes para ela, resete sua implementação e crie uma nova, que receba um parâmetro e retorne seu dobro. Faça os testes necessários.*/
+
+
+function randomNumber () {
+  return Math.floor(Math.random() * 11);
+}
+
+test('Teste do exercicio 3.', () => {
+  randomNumber = jest.fn().mockImplementation((a, b, c) => a * b * c);
+  expect(randomNumber(2, 2, 2)).toBe(8);
+  expect(randomNumber).toHaveBeenCalled();
+  expect(randomNumber).toBeCalledTimes(1);
+  expect(randomNumber).toHaveBeenCalledWith(2, 2, 2);
+});
+
+test('mockando função que recebe um parâmetro e retorna seu dobro.', () => {
+  randomNumber.mockRestore();
+  randomNumber.mockImplementation((a) => a * a);
+  expect(randomNumber(2)).toBe(4);
+  expect(randomNumber).toHaveBeenCalled();
+  expect(randomNumber).toBeCalledTimes(1);
+  expect(randomNumber).toHaveBeenCalledWith(2);
+});
